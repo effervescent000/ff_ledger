a8e82149e744fd4e2ac7b8e197e851378e202cc2
effervescent000 <kislio01@gmail.com>
2021-08-05 09:26:31 -0400

    Added HQ checkbox and moved from pack to grid

--- gui/new_material_window.py
+++ gui/new_material_window.py
@@ -7,15 +7,21 @@ class NewMaterialWindow:
         self.new_material_window = tk.Tk()
 
         self.material_name = tk.Entry(self.new_material_window)
+        self.material_hq_var = tk.IntVar(self.new_material_window)
+        self.material_hq_checkbox = tk.Checkbutton(self.new_material_window, text='HQ', variable=self.material_hq_var)
         self.material_price = tk.Entry(self.new_material_window)
 
         ok_button = tk.Button(self.new_material_window, text='Ok')
         cancel_button = tk.Button(self.new_material_window, text='Cancel', command=self.new_material_window.destroy)
 
-        self.material_name.pack()
-        self.material_price.pack()
-        ok_button.pack()
-        cancel_button.pack()
+        material_widgets = [self.material_name, self.material_hq_checkbox, self.material_price]
+        col = 0
+        for x in material_widgets:
+            x.grid(row=0, column=col)
+            col += 1
+
+        ok_button.grid(row=1, column=2)
+        cancel_button.grid(row=2, column=2)
 
         ok_button.bind('<Button-1>', self.save_new_material)
 
