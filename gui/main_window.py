2273872d37051dd7a742dab7c0ebf687318e5cc9
effervescent000 <kislio01@gmail.com>
2021-08-16 19:11:23 -0400

    Product and material comboboxes now autocomplete

--- gui/main_window.py
+++ gui/main_window.py
@@ -30,7 +30,9 @@ class MainWindow:
         self.chosen_product = tk.StringVar(self.upper_frame)
         self.chosen_product.set('Select a product')
         self.product_price_entry = tk.Entry(self.upper_frame)
-        self.product_combo = ttk.Combobox(self.upper_frame, textvariable=self.chosen_product, values=self.products)
+        self.product_combo = self.AutocompleteCombobox(self.upper_frame, textvariable=self.chosen_product,
+                                                       values=self.products)
+        self.product_combo.set_completion_list(self.products)
         self.product_hq_checkbox_var = tk.IntVar(self.upper_frame)
         self.product_hq_checkbox = tk.Checkbutton(self.upper_frame, text='HQ', variable=self.product_hq_checkbox_var)
         add_sale_button = tk.Button(self.upper_frame, text='Add sale')
@@ -75,7 +77,9 @@ class MainWindow:
         self.chosen_material = tk.StringVar(self.upper_frame)
         self.chosen_material.set('Select a material')
         self.material_price_entry = tk.Entry(self.upper_frame)
-        self.material_combo = ttk.Combobox(self.upper_frame, textvariable=self.chosen_material, values=self.materials)
+        self.material_combo = self.AutocompleteCombobox(self.upper_frame, textvariable=self.chosen_material,
+                                                        values=self.materials)
+        self.material_combo.set_completion_list(self.materials)
         self.material_hq_checkbox_var = tk.IntVar(self.upper_frame)
         self.material_hq_checkbox = tk.Checkbutton(self.upper_frame, text='HQ', variable=self.material_hq_checkbox_var)
         add_purchase = tk.Button(self.upper_frame, text='Add purchase')
@@ -237,3 +241,57 @@ class MainWindow:
 
         self.xp.save_xml()
         self.main_window.destroy()
+
+    class AutocompleteCombobox(ttk.Combobox):
+        """Code taken from https://mail.python.org/pipermail/tkinter-discuss/2012-January/003041.html"""
+
+        def set_completion_list(self, completion_list):
+            """Use our completion list as our drop down selection menu, arrows move through menu."""
+            self._completion_list = sorted(completion_list, key=str.lower)  # Work with a sorted list
+            self._hits = []
+            self._hit_index = 0
+            self.position = 0
+            self.bind('<KeyRelease>', self.handle_keyrelease)
+            self['values'] = self._completion_list  # Setup our popup menu
+
+        def autocomplete(self, delta=0):
+            """autocomplete the Combobox, delta may be 0/1/-1 to cycle through possible hits"""
+            if delta:  # need to delete selection otherwise we would fix the current position
+                self.delete(self.position, tk.END)
+            else:  # set position to end so selection starts where textentry ended
+                self.position = len(self.get())
+            # collect hits
+            _hits = []
+            for element in self._completion_list:
+                if element.lower().startswith(self.get().lower()):  # Match case insensitively
+                    _hits.append(element)
+            # if we have a new hit list, keep this in mind
+            if _hits != self._hits:
+                self._hit_index = 0
+                self._hits = _hits
+            # only allow cycling if we are in a known hit list
+            if _hits == self._hits and self._hits:
+                self._hit_index = (self._hit_index + delta) % len(self._hits)
+            # now finally perform the auto completion
+            if self._hits:
+                self.delete(0, tk.END)
+                self.insert(0, self._hits[self._hit_index])
+                self.select_range(self.position, tk.END)
+
+        def handle_keyrelease(self, event):
+            """event handler for the keyrelease event on this widget"""
+            if event.keysym == "BackSpace":
+                self.delete(self.index(tk.INSERT), tk.END)
+                self.position = self.index(tk.END)
+            if event.keysym == "Left":
+                if self.position < self.index(tk.END):  # delete the selection
+                    self.delete(self.position, tk.END)
+                else:
+                    self.position = self.position - 1  # delete one character
+                    self.delete(self.position, tk.END)
+            if event.keysym == "Right":
+                self.position = self.index(tk.END)  # go to end (no selection)
+            if len(event.keysym) == 1:
+                self.autocomplete()
+            # No need for up/down, we'll jump to the popup
+            # list at the position of the autocompletion
