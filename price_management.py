2a5b5ae3a6821bb4715d2ac965a1d0cf72c519ad
effervescent000 <kislio01@gmail.com>
2021-08-25 10:24:02 -0400

    Added (NYI) price_management.py

---
+++ price_management.py
@@ -0,0 +1,8 @@
+def purge_old_price_data(text_box=None):
+    """Removes price data from items with more than x price points, or removes price points older than y days"""
+    pass
+
+
+def find_old_prices(text_box=None):
+    """Finds items without recent price data"""
+    pass
